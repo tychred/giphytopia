ff65ce86fc45f90305931f2d8758ad36dcf98c6e
tychred <tyler.edmunds@gmail.com>
2024-08-06 14:17:05 -0600

    updated script.js and added to index.html

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-console.log("hello there!")
+"use strict";
+
+async function getTrendingGifs() {
+    const url = "https://api.giphy.com/v1/gifs/trending?api_key=QAHMQrTIXX19lY4ctkG18ZR4UY9PwDks&limit=5";
+    try {
+     const response = await fetch(url);
+     if (!response.ok) {
+         throw new Error('Response status: ${response status}');
+     }
+     const giphys = await response.json();
+     return giphys.data;
+    } catch (error) {
+     console.error(error.message);
+    }
+ }
+ 
+ async function renderImage() {
+     const myGif = await getTrendingGifs()
+     console.log(myGif)
+     // console.log(`<img src="${myGif}" alt="first gif">`)
+     // document.getElementById("first-gif").innerHTML = `<img src=${myGif} alt="first gif">`
+     const gifMap = await myGif.map(gif => {
+         console.log(gif)
+         //TODO: Append each gif to the DOM
+     })
+ }
+ renderImage()
+
+
+
+
+
+
+
+
+
+// const numbers = [1, 2, 3, 4, 5];
+ 
+// function squareNumbers(arr) {
+//   return arr.map((number) => {
+//     return Math.pow(number, 2)
+//   });
+// }
+ 
+// console.log(squareNumbers(numbers)); // Expected output: [1, 4, 9, 16, 25]
+
+
+
+
+
+
